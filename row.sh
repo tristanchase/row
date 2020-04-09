2245bafd4767dd8bae2bf0c38aed427487416fbf
Tristan Chase <tristan.m.chase@gmail.com>
2020-04-08 21:42:20 -0400

    Change /tmp to "$HOME"/tmp
    And remove trailing whitespace.

--- row.sh
+++ row.sh
@@ -7,7 +7,7 @@
 # Invoke as !command `row n`<Enter>.
 #
 # Matches line number from ${outfile} (which is a temp file created
-# by loco in /tmp/loco.$$) with argument $1 and returns the path on 
+# by loco in "$HOME"/tmp/loco.$$) with argument $1 and returns the path on
 # that line.  Need to handle paths with spaces in the names.
 #
 # Need to test $1 for numbers only (and maybe range of lines in ${outfile}).
